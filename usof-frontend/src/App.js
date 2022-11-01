62cc2e2fba4c7e4b69ae0568f1a297464e26bb07
Vlad-Makarenko <72661192+Vlad-Makarenko@users.noreply.github.com>
2022-11-01 19:16:46 +0200

    Update App.js

--- usof-frontend/src/App.js
+++ usof-frontend/src/App.js
@@ -9,7 +9,6 @@ import { useRoutes } from './hooks/routes.hook';
 import { NavBar } from './components/NavBar';
 
 import './App.css';
-import { useMessage } from './hooks/message.hook';
 import { checkAuth } from './store/authSlice';
 import { LeftSideBar } from './components/LeftSideBar';
 import { useModal } from './hooks/modal.hook';
@@ -28,35 +27,12 @@ const App = () => {
   const routes = useRoutes();
   const modals = useModal();
   const dispatch = useDispatch();
-  const { error: postErr } = useSelector((state) => state.post);
-  const { error: tagErr } = useSelector((state) => state.tag);
-  const { error: authErr } = useSelector((state) => state.auth);
-  const { error: userErr } = useSelector((state) => state.user);
-  const { error: CommentErr } = useSelector((state) => state.comment);
-
+  
   useEffect(() => {
     if (localStorage.getItem('token')) {
       dispatch(checkAuth());
     }
   }, []);
-
-  const message = useMessage();
-
-  useEffect(() => {
-    message(postErr, 'error');
-  }, [postErr]);
-  useEffect(() => {
-    message(tagErr, 'error');
-  }, [tagErr]);
-  useEffect(() => {
-    message(authErr, 'error');
-  }, [authErr]);
-  useEffect(() => {
-    message(userErr, 'error');
-  }, [userErr]);
-  useEffect(() => {
-    message(CommentErr, 'error');
-  }, [CommentErr]);
 
   return (
     <Router>
