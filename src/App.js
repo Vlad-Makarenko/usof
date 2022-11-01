a49c5ad13631796e3b2a760aa66c60e7fa742af4
Vlad-Makarenko <Makrenkovladok@gmail.com>
2022-11-01 19:02:42 +0200

    deploy 1.1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
-import { Col, Container, Row } from 'react-bootstrap';
+import {
+  Col, Container, Row, Spinner,
+} from 'react-bootstrap';
 import { useRoutes } from './hooks/routes.hook';
 import { NavBar } from './components/NavBar';
 
 import './App.css';
-import { useMessage } from './hooks/message.hook';
 import { checkAuth } from './store/authSlice';
 import { LeftSideBar } from './components/LeftSideBar';
 import { useModal } from './hooks/modal.hook';
@@ -28,11 +29,7 @@ const App = () => {
   const routes = useRoutes();
   const modals = useModal();
   const dispatch = useDispatch();
-  const { error: postErr } = useSelector((state) => state.post);
-  const { error: tagErr } = useSelector((state) => state.tag);
-  const { error: authErr } = useSelector((state) => state.auth);
-  const { error: userErr } = useSelector((state) => state.user);
-  const { error: CommentErr } = useSelector((state) => state.comment);
+  const { isLoading } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -40,23 +37,9 @@ const App = () => {
     }
   }, []);
 
-  const message = useMessage();
-
-  useEffect(() => {
-    message(postErr, 'error');
-  }, [postErr]);
-  useEffect(() => {
-    message(tagErr, 'error');
-  }, [tagErr]);
-  useEffect(() => {
-    message(authErr, 'error');
-  }, [authErr]);
-  useEffect(() => {
-    message(userErr, 'error');
-  }, [userErr]);
-  useEffect(() => {
-    message(CommentErr, 'error');
-  }, [CommentErr]);
+  if (isLoading) {
+    return <Spinner animation="border" variant="warning" />;
+  }
 
   return (
     <Router>
